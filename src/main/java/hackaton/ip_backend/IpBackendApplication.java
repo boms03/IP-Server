1d2e9c6aabbc21c8d65538790460421638ac6617
yuuddin <jindf51@naver.com>
2024-06-01 13:45:45 +0900

    [CHORE] 프로젝트 세팅

---
+++ src/main/java/hackaton/ip_backend/IpBackendApplication.java
@@ -0,0 +1,13 @@
+package hackaton.ip_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpBackendApplication.class, args);
+	}
+
+}
