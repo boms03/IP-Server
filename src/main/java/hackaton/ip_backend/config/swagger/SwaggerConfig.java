ec992c98f5e834285bcf19472e9fb26fd5db7b75
destiny3912 <destiny3910@gmail.com>
2024-06-02 00:43:44 +0900

    [Feat] #25 스웨거 토큰 헤더 추가

--- src/main/java/hackaton/ip_backend/config/swagger/SwaggerConfig.java
+++ src/main/java/hackaton/ip_backend/config/swagger/SwaggerConfig.java
@@ -3,10 +3,13 @@ package hackaton.ip_backend.config.swagger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
@@ -20,8 +23,18 @@ public class SwaggerConfig {
 				.title("IP Service API Documentation")
 				.version(version);
 
+		SecurityScheme securityScheme = new SecurityScheme()
+				.type(SecurityScheme.Type.HTTP)
+				.bearerFormat(HttpHeaders.AUTHORIZATION)
+				.in(SecurityScheme.In.HEADER)
+				.name(HttpHeaders.AUTHORIZATION);
+
+		SecurityRequirement requirement = new SecurityRequirement();
+		requirement.addList(HttpHeaders.AUTHORIZATION);
+
 		return new OpenAPI()
 				.components(new Components())
+				.addSecurityItem(requirement)
 				.info(info);
 	}
 
